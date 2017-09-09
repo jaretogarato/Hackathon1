ee1e73b46105b4686e91411687cbc4014abb96d9
Jared Gallardo <jaredgg@gmail.com>
2017-09-09 16:13:28 -0600

    basic functionality for the app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show]
+
   def index
     @posts = Post.all
   end
 
   def show
+
   end
 
   def new
@@ -31,6 +34,9 @@ class PostsController < ApplicationController
   end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:content, :title)
