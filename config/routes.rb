90acd4486997ce0a5fcd895b920ee113a18c4e6a
Jared Gallardo <jaredgg@gmail.com>
2017-09-09 13:16:38 -0600

    minor patch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # root 'users#index'
-  root to: 'accounts#index'
+  root to: 'posts#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
